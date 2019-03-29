d98467ee4f92137b2a96f597c69ffc4f17359fd1
KMikalsen <KMikalsen@users.noreply.github.com>
2019-03-29 12:29:48 +0100

    Added tutorial image

--- core/src/com/mygdx/game/states/TutorialState.java
+++ core/src/com/mygdx/game/states/TutorialState.java
@@ -6,27 +6,17 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.mygdx.game.TankGame;
-
-import java.nio.file.Files;
-
 
 public class TutorialState extends State {
     private Texture bg;
-    //private Texture optionTitle;
+    private Texture tutorialGuiHelp;
     private Image homeBtn;
     private Stage stage;
     private BitmapFont tutorialFont;
@@ -37,12 +27,13 @@ public class TutorialState extends State {
         cam.setToOrtho(false, TankGame.WIDTH, TankGame.HEIGHT);
         bg = new Texture("bg.png");
 
-        //optionTitle = new Texture("optionTitle.png");
+        tutorialGuiHelp = new Texture("tutorial_bottom_gui.png");
+
         stage = new Stage(new ScreenViewport());
 
         if(fromMenuState){
             homeBtn = new Image(new Texture("homeBtn.png"));
-            homeBtn.setSize(homeBtn.getWidth(), homeBtn.getHeight());
+            homeBtn.setSize(homeBtn.getWidth() *0.8f, homeBtn.getHeight() * 0.8f);
             homeBtn.setPosition(TankGame.WIDTH * 0.1f,TankGame.HEIGHT - (homeBtn.getHeight() + 10 ));
             stage.addActor(homeBtn);
         }
@@ -65,9 +56,6 @@ public class TutorialState extends State {
         tutorialFont = generator.generateFont(parameter);
         generator.dispose();
 
-        //Should maybe load tutorial text in from external file?
-        //new String(Files.readAllBytes(...)) or Files.lines(..).forEach(...) requires a higher Android API than we're using?
-
         FileHandle file = Gdx.files.internal("tutorialText.txt");
         tutorialContent = file.readString();
     }
@@ -87,11 +75,11 @@ public class TutorialState extends State {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
         sb.draw(bg, 0,0, 1280, 720);
-        //sb.draw(optionTitle, cam.position.x - optionTitle.getWidth()/2, 500+optionTitle.getHeight()/4);
+        sb.draw(tutorialGuiHelp, TankGame.WIDTH*0.2f, 10);
         tutorialFont.draw(sb,
                 tutorialContent,
                 TankGame.WIDTH * 0.1f,
-                600,
+                640,
                 TankGame.WIDTH * 0.8f ,
                 8,
                 true);
