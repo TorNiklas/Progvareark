3e552dcee72ea695915695608e900fc99a2b6626
JensTK <GA629tamm>
2019-05-08 14:17:04 +0200

    removed unused files

--- core/src/com/mygdx/game/logic/Game.java
+++
@@ -1,5 +0,0 @@
-package com.mygdx.game.logic;
-
-public class Game {
-    private long milTimer = 10000;
-}
--- core/src/com/mygdx/game/logic/Player.java
+++
@@ -1,8 +0,0 @@
-package com.mygdx.game.logic;
-
-import com.mygdx.game.sprites.Tank;
-
-public class Player {
-    private int health = 0;
-    private int energy = 10;
-}
